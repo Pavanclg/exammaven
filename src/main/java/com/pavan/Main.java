b8b1e568db3a3ba85a96f15a5d17dcfdaea5e696
Pavanclg <pavanatwork10@gmail.com>
2026-06-29 23:43:30 +0530

    Update Main.java

--- src/main/java/com/pavan/Main.java
+++ src/main/java/com/pavan/Main.java
@@ -1,8 +1,12 @@
 package com.pavan;
-public class Main {
-    public static void main(String[] args)
-    {
-        System.out.println("hello CI/CD pipeline");
-    }
 
+public class Main {
+    public static void main(String[] args) throws Exception {
+
+        System.out.println("Application Started");
+
+        while (true) {
+            Thread.sleep(10000);
+        }
+    }
 }
